ff941946f5ccd25a039397d705a4a91cb812b290
Jerricke <eejerrickw@gmail.com>
2023-06-13 12:26:53 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const mainRemove = document.getElementById('main');
+mainRemove.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.textContent= "You is the champion"
